a6c7a086e4ab4204f964173d2b389d77a2680aaa
stdai1016 <stdai1016@gmail.com>
2021-03-01 23:02:59 +0800

    Append setting page

--- plurk_shadow_block.user.js
+++ plurk_shadow_block.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Plurk shadow block
 // @name:zh-tw   噗浪隱形黑名單
-// @version      0.1.0
+// @version      0.2.0
 // @description  Shadow blocks user (only blocks on responses and timeline of yourself)
 // @description:zh-tw 隱形封鎖使用者（只是會在回應和在河道上看不到被封鎖者的發文、轉噗，其他正常）
 // @match        https://www.plurk.com/*
@@ -43,7 +43,7 @@
     '.resp-hidden-show:hover {background:#afb8cc;color:#fff}'
   );
 
-  if (window.location.pathname.match(/^\/p\/[A-Za-z\d]+$/)) {
+  if (window.location.pathname.match(/^\/p\/[A-Za-z]\w+$/)) {
     // responses
     const respMo = new MutationObserver(responseMutationHandler);
     respMo.observe($('#plurk_responses .list')[0], { childList: true });
@@ -52,6 +52,39 @@
     const cboxMo = new MutationObserver(responseMutationHandler);
     cboxMo.observe($('#cbox_response .list')[0], { childList: true });
     makeButton($('#cbox_response>.response_box'));
+  } else if (window.location.pathname === '/Friends/') {
+    $('<li><a void="">隱形黑名單</a></li>').on('click', function () {
+      window.history.pushState('', document.title, '/Friends/');
+      $('#pop-window-tabs>ul>li').removeClass('current');
+      this.classList.add('current');
+      const $content = $('#pop-window-inner-content .content_inner').empty();
+      $content.append(
+        '<div class="note">在回應區和自己的河道上看不到被封鎖者的發文、轉噗</div>',
+        '<div class="dashboard">' +
+        ' <div class="search_box"><input><button>新增</button></div>' +
+        ' <div class="empty">目前沒有任何人在黑名單中</div>' +
+        '</div>');
+      const $holder = $('<div class="item_holder"></div>').appendTo($content);
+      const usersInfo = [];
+      valueGetSet('blocklist').forEach(id => usersInfo.push(getUserInfo(id)));
+      if (usersInfo.length) {
+        $content.find('.dashboard .empty').addClass('blocked');
+      }
+      Promise.all(usersInfo).then(infomations => infomations.forEach(info => {
+        makeBlockedUserItem(info, $holder);
+      }));
+      $content.find('.search_box>button').on('click', function () {
+        const m = this.parentElement.children[0].value.match(/^[A-Za-z]\w+$/);
+        if (m) {
+          const blocklist = valueGetSet('blocklist');
+          blocklist.push(m[0]);
+          valueGetSet('blocklist', blocklist);
+          this.parentElement.children[0].value = '';
+          $content.find('.dashboard .empty').addClass('blocked');
+          getUserInfo(m[0]).then(info => makeBlockedUserItem(info, $holder));
+        } else { window.alert('帳號格式不正確'); }
+      });
+    }).appendTo('#pop-window-tabs>ul');
   } else if ($('#nav-account>span').text() ===
       window.location.pathname.substr(1)) {
     // timeline
@@ -107,6 +140,57 @@
         .toggleClass('blocked');
     }).addClass(['resp-hidden-show', 'button', 'small-button', 'blocked'])
       .insertAfter($responseBox.find('.response-only-owner'));
+  }
+
+  function makeBlockedUserItem (info, holder) {
+    const $u = $('<div class="user_item user_blocked_users_item"></div>');
+    $u.append(
+      '<a class="user_avatar" target="_blank">' +
+        '<img class="profile_pic" src="' + info.img + '"></img></a>',
+      '<div class="user_info">' +
+      '  <a class="user_link" target="_blank">' + info.name + '</a>' +
+      '  <span class="nick_name">@' + info.id + '</span>' +
+      '  <div class="more_info"><br></div>' +
+      '</div>',
+      '<div class="user_action"><a void="" data-id="' + info.id + '" ' +
+        'class="friend_man icon_only pif-user-blocked has_block" ' +
+        'title="移除"></a></div>'
+    );
+    $u.find('a:not(.has_block)').attr('href', '/' + info.id);
+    $u.find('a.has_block').on('click', function () {
+      const blocklist = valueGetSet('blocklist');
+      for (let i = 0; i < blocklist.length; ++i) {
+        if (blocklist[i] === this.dataset.id) {
+          blocklist.splice(i, 1);
+          valueGetSet('blocklist', blocklist);
+          $u.remove();
+          break;
+        }
+      }
+    });
+    $u.appendTo(holder);
+  }
+
+  function getUserInfo (id) {
+    return new Promise((resolve, reject) => {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'document';
+        xhr.onload = function () {
+          const h = xhr.responseXML.head;
+          const title = h.querySelector('[property="og:title"]').content;
+          const img = h.querySelector('[property="og:image"]').content;
+          const name =
+            (title.match(/(.+) \[.+\]/) || title.match(/(.+) - Plurk/))[1];
+          resolve({ id: id, name: name, img: img });
+        };
+        xhr.onerror = function () {
+          resolve({ id: id, name: id, img: null });
+        };
+        xhr.open('GET', 'https://www.plurk.com/' + id);
+        xhr.send();
+      } catch (e) { resolve({ id: id, name: id, img: null }); }
+    });
   }
 
   function onBlockList (user) {
